9ae822da62fbeb95de9fb9f63077e8451b592a4c
Vlad <42578442+crates51@users.noreply.github.com>
2018-08-24 18:55:05 +0200

    Initializing the project 1/2
    The Begining

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="includes/code/css/mainStyle.css">
+		<title>Casa Jasmin</title>
+	</head>
+
+	<body>
+		<header>
+			<span>Casa Jasmin</span>
+		</header>
+
+		<nav>
+			The Navigation Bar
+		</nav>
+
+		<section>
+			The content
+		</section>
+	</body>
+</html>
