f3d11c59c6351cc5a554f76b20076635f6ae34cf
crates51 <musteatavlad@yahoo.com>
2018-08-25 12:51:56 +0200

    Improved NavBar&Started Carousel

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,32 @@ var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
 
+var plumber     = require('gulp-plumber');
+// var coffee      = require('gulp-coffee');
+
+// function swallowError (error) {
+
+//   // If you want details of the error in the console
+//   console.log(error.toString())
+
+//   this.emit('end')
+// }
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
     return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+        .pipe(plumber())
         .pipe(sass())
         .pipe(gulp.dest("src/css"))
+        // .on('error', swallowError)
         .pipe(browserSync.stream());
 });
 
 // Move the javascript files into our /src/js folder
 gulp.task('js', function() {
     return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
+        .pipe(plumber())
         .pipe(gulp.dest("src/js"))
+        // .on('error', swallowError)
         .pipe(browserSync.stream());
 });
 
